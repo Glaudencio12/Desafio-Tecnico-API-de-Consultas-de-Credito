ca29bf34cf1736ec75e430e37c229a225c17b5b7
Glaudencio12 <glaudenciocosta015@gmail.com>
2025-07-12 09:25:34 -0300

    :tada: init: Commit Inicial

---
+++ src/test/java/com/glaudencio12/Consulta_de_Creditos/ConsultaDeCreditosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glaudencio12.Consulta_de_Creditos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaDeCreditosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
